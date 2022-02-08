1906612d79f5c4ada2dbbd3d70d758be7fc8f386
STARSZYPAN <98351502+STARSZYPAN@users.noreply.github.com>
2022-02-08 21:23:20 +0100

    add func

--- magazyn.py
+++ magazyn.py
@@ -1,80 +1,112 @@
 
 user_choice = -1
 
-tasks=[]
-tasks.append('Wynieśc śmieci')
-tasks.append('Posprzątać biurko')
+names=[]
 
-def show_tasks():
-    task_index= 0
-    for task in tasks:
-        print(task + " [" + str(task_index) + "]")
-        task_index += 1
+quantitys=[]
+
+units=[]
+
+prices=[]
+
+
+
+
+
+
+print("Co chcesz teraz zrobić?: ")
+
+def show_produkt():   
+    names_index=0
+    for name in names:
+        print(name + "["+ str(names_index) + "]")
+        names_index += 1
+  
+    
         
-def add_tasks():
-        task=input('Wpisz treść zadania:  ')
-        tasks.append(task)
-        print('Dodano zadanie')
+def add_produkt():
+        name=input('Wpisz nazwę produktu:  ')
+        names.append(name)
+        print('Dodano produkt')
+#def add_quantity():
+        #quantity=input('Wpisz ilość produktu:  ')
+        #quantitys.append(quantity)
+        #print('Dodano ilość produktu')
+#def add_unit():
+        #unit=input('Wpisz jednostkę  miary produktu:  ')
+        #units.append(unit)
+        #print('Dodano jednostkę miary')
+#def add_price():
+        #price=input('Wpisz cenę produktu:  ')
+        #price.sappend(price)
+        #print('Dodano cenę')
 
-def delete_tasks():
-    task_index = int(input('Podaj indeks zadania do usunięcia:'))
+def delete_produkty():
+    produkt_index = int(input('Podaj indeks produktu do usunięcia:'))
 
-    if task_index <0 or task_index > len(tasks) -1:
-        print('zadanie o tym indeksie nie istnieje')
+    if produkt_index <0 or produkt_index > len(names) -1:
+        print('produkt o tym indeksie nie istnieje')
         return
 
-    tasks.pop(task_index)
-    print('Usunięto zadanie')
+    names.pop(produkt_index)
+    print('Usunięto produkt')
 
-def save_task_to_file():
-    file = open('tasks.txt' , 'w')
+def save_produkt_to_file():
+    file = open('produkty.txt' , 'w')
 
-    for task in tasks:2
+    for name in names:2
 
-    file.write(task + "\n")
+    file.write(name + "\n")
     file.close()
 
-def load_tasks_from_file():
+def load_produkty_from_file():
     try:
 
-        file = open ('tasks.txd')
+        file = open ('produkty.txd')
 
         for line in file.readlines():
-           tasks.append(line.strip())
+           names.append(line.strip())
 
         file.close()
 
     except FileNotFoundError:
         return
-
-load_tasks_from_file()
+def dowidzenia():
+    exit
+load_produkty_from_file()
 
 
 
 
 while user_choice !=5:
     if user_choice==1:
-        show_tasks()
+        
+        print("Name\tQuantity\tUnit\tUnit Price (PLN)\tIndex")
+        print("----\t--------\t----\t----------------\t------")
+        show_produkt()
 
     if user_choice==2:
-        add_tasks()
+        add_produkt()
 
     if user_choice==3:
-        delete_tasks()
+        delete_produkty()
 
     if user_choice==4:
-       save_task_to_file()
+       save_produkt_to_file()
+    
+    if user_choice==5:
+       dowidzenia()
     
     
 
 
 
     print()
-    print('1.Pokaz zadania')
-    print('2.Dodajzadanie')
-    print('3.Usuń zadanie')
-    print('4.Zapisz zmiany w pliku')
+    print('1.Pokaz towary')
+    print('2.Dodaj towary')
+    print('3.Usuń towary')
+    print('4.Zapisz zmiany w magazynie')
     print('5.Wyjdź')
 
-    user_choice=int(input('Wybierz liczbę: '))
+    user_choice=int(input('Wybierz operację: '))
     print()
